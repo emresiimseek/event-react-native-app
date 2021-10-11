2c17489dca81b670f7507ebec3e815b07098e56e
emresimsek <emresimseka@gmail.com>
2021-10-11 19:40:59 +0300

    feature(plugin-react-navigation):

--- App.js
+++ App.js
@@ -2,10 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomePage from './app/screens/WelcomePage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Button from './app/common-components/Button';
+import LoginPage from './app/screens/LoginPage';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <WelcomePage />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={WelcomePage} >
+        <Stack.Screen name="Welcome" component={WelcomePage}
+          options={{
+            headerStyle: { backgroundColor: '#00000050', },
+            headerTitleStyle: { color: "white", },
+            title: 'BeSocial',
+          }} />
+        <Stack.Screen name="Login" component={LoginPage}
+          options={{
+            headerStyle: { backgroundColor: '#00000050', },
+            headerTitleStyle: { color: "white", },
+            title: 'Giriş',
+          }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
